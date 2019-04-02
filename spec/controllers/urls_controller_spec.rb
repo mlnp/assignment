a9392f495307735b8adc29ed676c02c997ce93a0
Jeremy Lenz <jeremylenz@gmail.com>
2019-04-02 17:58:22 -0400

    improve test descriptions

--- spec/controllers/urls_controller_spec.rb
+++ spec/controllers/urls_controller_spec.rb
@@ -9,12 +9,12 @@ RSpec.describe UrlsController, type: :controller do
       })
   }
   describe "GET show" do
-    it "responds with 302 if a valid url is present" do
+    it "responds with 302 Found if a valid url is present" do
       subject.save
       get :show, params: {code: 'exampl'}
       expect(response.status).to eq(302)
     end
-    it "responds with 404 when url is not found" do
+    it "responds with 404 Not Found when url is not found" do
       get :show, params: {code: 'n0n0n0'}
       expect(response.status).to eq(404)
     end
@@ -36,36 +36,36 @@ RSpec.describe UrlsController, type: :controller do
       expect(received_stats.keys).not_to include("last_usage")
     end
 
-    it "responds with 404 when url is not found" do
+    it "responds with 404 Not Found when url is not found" do
       get :stats, params: {code: 'n0n0n0'}
       expect(response.status).to eq(404)
     end
   end
 
-  describe "GET create" do
-    it "creates a url and responds with 201 when given valid params" do
-      get :create, params: subject.attributes
+  describe "POST create" do
+    it "creates a url and responds with 201 Created when given valid params" do
+      post :create, params: {code: subject.shortcode, url: subject.url}
       expect(response.status).to eq(201)
     end
 
-    it "responds with 409 if shortcode is already taken" do
-      subject.save # so shortcode will be taken already
-      get :create, params: subject.attributes
+    it "responds with 409 Conflict if shortcode is already taken" do
+      existing_url = subject.save # so shortcode will be taken already
+      post :create, params: {code: subject.shortcode, url: subject.url}
       expect(response.status).to eq(409)
     end
 
-    it "responds with 400 if url is missing" do
-      get :create, params: {shortcode: subject.shortcode}
-      expect(response.status).to eq(409)
-    end
-
-    it "responds with 400 if url is invalid" do
-      get :create, params: {shortcode: subject.shortcode, url: "invalid url"}
+    it "responds with 400 Bad Request if url is missing" do
+      post :create, params: {code: subject.shortcode}
       expect(response.status).to eq(400)
     end
 
-    it "responds with 422 if shortcode is invalid" do
-      get :create, params: {url: subject.url, shortcode: "$%^&*7"}
+    it "responds with 400 if url is invalid" do
+      post :create, params: {code: subject.shortcode, url: "invalid url"}
+      expect(response.status).to eq(400)
+    end
+
+    it "responds with 422 Unprocessable Entity if shortcode is invalid" do
+      post :create, params: {url: subject.url, code: "$%^&*7"}
       expect(response.status).to eq(422)
     end
   end
