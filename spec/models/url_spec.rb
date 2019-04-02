4fa2fc2cf7a9b84b4960f17fed199fcb40b4f4da
Jeremy Lenz <jeremylenz@gmail.com>
2019-04-02 19:04:03 -0400

    Add specs for add_url_protocol

--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -33,6 +33,40 @@ RSpec.describe Url, type: :model do
       subject.shortcode = "jsidofijsodjifos"
       expect(subject).not_to be_valid
     end
+  end
 
+  describe "methods" do
+    it "generates a shortcode of length 6" do
+      subject.shortcode = nil
+      subject.generate_shortcode
+      expect(subject.shortcode).not_to be_nil
+      expect(subject.shortcode.length).to eq(6)
+    end
+
+    it "increments its usage count" do
+      subject.save
+      expect(subject.usage_count).to eq(0)
+      expect(subject.last_usage).to be_nil
+      subject.record_usage
+      expect(subject.usage_count).to eq(1)
+      expect(subject.last_usage).not_to be_nil
+    end
+
+    it "adds http:// to url if not present" do
+      subject.url = "www.example.com"
+      subject.validate
+      expect(subject.url).to eq("http://www.example.com")
+    end
+
+    it "does not add http:// to url if http:// is already present" do
+      subject.validate
+      expect(subject.url).to eq("http://www.example.com")
+    end
+
+    it "does not add http:// to url if https:// is already present" do
+      subject.url = "https://www.example.com"
+      subject.validate
+      expect(subject.url).to eq("https://www.example.com")
+    end
   end
 end
